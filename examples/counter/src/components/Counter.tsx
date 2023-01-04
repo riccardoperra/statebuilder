523fc6b29fde2451e8ca378e36e675d898ec3c0c
riccardoperra <riccardo.perra@icloud.com>
2023-01-05 00:10:12 +0100

    chore: add counter example

--- examples/counter/src/components/Counter.tsx
+++ examples/counter/src/components/Counter.tsx
@@ -1,15 +1,41 @@
 import './Counter.css';
-import { defineStore } from 'rstate';
+import { defineStore, provideState, withAsyncAction } from '../../../../src';
+import { createEffect } from 'solid-js';
 
-export default function Counter() {
-  const store = defineStore({
-    count: 0,
+const $store = defineStore({
+  count: 0,
+})
+  .extend(withAsyncAction())
+  .extend(ctx => {
+    return {
+      incrementAfter1S: ctx.asyncAction(async (payload: number) => {
+        console.log(payload);
+        await new Promise(r => setTimeout(r, 3000));
+        ctx.set('count', count => count + 1);
+        return `${payload} - test`;
+      }),
+    };
   });
 
+export default function Counter() {
+  const store = provideState($store);
+
+  createEffect(() => console.log('loadign', store.incrementAfter1S.loading));
+
   return (
-    <button class='increment'
-            onClick={() => store.set('count', count => count + 1)}>
-      Clicks: {store.state.count}
-    </button>
+    <>
+      <button
+        class='increment'
+        onClick={() => store.set('count', count => count + 1)}>
+        Clicks: {store().count}
+      </button>
+
+      <button
+        class='increment'
+        onClick={() => store.incrementAfter1S()}>
+        After 1 sec
+      </button>
+    </>
+
   );
 }
