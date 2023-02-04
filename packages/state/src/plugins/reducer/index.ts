4218ac82f819f269d9d0a48f19c7b8de0d87082b
Riccardo Perra <perrariccardo0@gmail.com>
2023-02-04 19:39:56 +0100

    feat: reducer plugin

---
+++ packages/state/src/plugins/reducer/index.ts
@@ -0,0 +1,27 @@
+import { makePlugin } from '~/api';
+import { Store } from '~/store';
+import { Signal } from '~/signal';
+import { GenericStoreApi } from '~/types';
+
+interface StoreWithReducer<T, Action> {
+  dispatch(action: Action): void;
+}
+
+type Reducer<State, Action> = (state: State, action: Action) => State;
+
+function reducerPlugin<TState, TAction>(
+  reducer: Reducer<TState, TAction>,
+): <TGenericApi extends GenericStoreApi<TState, any>>(
+  ctx: TGenericApi,
+) => StoreWithReducer<TState, TAction> {
+  return (ctx) => {
+    return {
+      dispatch(action: TAction) {
+        ctx.set((prevState: TState) => reducer(prevState, action));
+      },
+    };
+  };
+}
+
+export const withReducer = <S, A>(reducer: (state: S, action: A) => S) =>
+  makePlugin((store) => reducerPlugin(reducer)(store), { name: 'withReducer' });
